37c770bfee5cadcc9a76436a14e974a6e789af82
mslim <mslim0202@hanmail.net>
2024-09-01 17:08:04 +0900

    ver3.0

--- coinbot_ver3.0.py
+++ coinbot_ver3.0.py
@@ -10,7 +10,37 @@ import schedule
 import datetime
 import traceback
 import os
-from fucntion import trade
+from fucntion import trade , indicator
 
-## 호가 창(oreder book)의 ask_price 기준 현재가 가져옴. 시장가 매수 진행
-## 매도는 subprocess 로 수행
+access_key = ""
+secret_key = ""
+
+## 업비트 인증
+bit = Upbit(access_key, secret_key)
+
+
+if __name__ == '__main__':
+    try:
+        last_list=indicator.tickerfinder()
+        schedule.every(4).hours.do(indicator.tickerfinder())
+        while True:
+            try:
+                schedule.run_pending()
+                for i in last_list:
+                    st_rsi= indicator.stocatic_rsi(i,9,3,9,26,12)
+                    time.sleep(0.2)
+                    ## return  값 [0] = slwo_k , [1] = slow_d , [2] = RSI , [3] = MACD , [4] = MACD 기준선, [5] = obv , [6]= obv 이평선 , [7] = 볼린저 upper,[8] = 볼린저 lower , [9] = pdi , [10] = mdi , [11] = adx 
+                    if   st_rsi[2] < 50 and st_rsi[9] > st_rsi[10] and st_rsi[9] > st_rsi[11] and st_rsi[10] < st_rsi[11]: 
+                        buyinfo=trade.buy_sell(i,st_rsi[7],st_rsi[8])
+                        if buyinfo == 0 :
+                            pass
+                        else:
+                            print(buyinfo,datetime.datetime.now())
+            except:
+                traceback.print_exc()
+                print(datetime.datetime.now())
+                pass
+    except:
+        traceback.print_exc()
+        print(datetime.datetime.now())
+        pass 
